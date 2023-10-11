9919c632f732490fdbfaaf8859d5776290fb481e
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-10-11 10:58:14 +0330

    chore: configure TypeORM and nextjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        if (config.get<string>('NODE_ENV') === "production") return {
+          type: 'postgres',
+          host: config.get<string>("DATABASE_HOST"),
+          port: config.get<number>("DATABASE_PORT"),
+          username: config.get<string>("DATABASE_USER"),
+          password: config.get<string>("DATABASE_PASSWORD"),
+          database: config.get<string>("DATABASE_NAME"),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        }
+        return {
+          type: "sqlite",
+          database: '.dev/database.sqlite',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        }
+      }
+    })
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
