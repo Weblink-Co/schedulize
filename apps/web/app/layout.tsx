240e1f52fbeb08cd4e19d85a043cf05d4684242d
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-10-13 16:54:47 +0330

    chore: install and configure shadcn and tailwind on web app

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,7 @@
+import "ui/styles/globals.css";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Turborepo",
@@ -16,7 +15,9 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
