5dfe8032a3847bc46cbd37ad63e895a291c0e1de
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-10-23 15:57:28 +0330

    bad: add some examples

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,8 @@
+import useTranslation from 'next-translate/useTranslation'
+
 export default function Page(): JSX.Element {
-  return (
-    <p className="text-red-400">test</p>
-  );
+  const { t } = useTranslation();
+    return (
+    <p className="text-red-400">{t("hello")}</p>
+    );
 }
