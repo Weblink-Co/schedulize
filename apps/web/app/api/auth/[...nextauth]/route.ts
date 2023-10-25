0addb5675cbbe85cd660726f75296fed4be1b8f5
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-10-25 17:05:50 +0330

    feat: add sign in mechanism with google and email

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth";
 import Email from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "@schedulize/prisma";
 
 const nextAuthOptions: NextAuthOptions = {
@@ -10,11 +11,20 @@ const nextAuthOptions: NextAuthOptions = {
     Email({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM
+    }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.email) return false;
+      const userInDB = await prisma.user.findUnique({ where: { email: user.email } });
+      if (!userInDB) return false;
+      return true;
+    }
+  }
 };
 
 
