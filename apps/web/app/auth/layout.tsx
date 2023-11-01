2f20c0aef879a6cb1031e79984a00863d89eb662
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-11-01 18:24:39 +0330

    chore: create a layout for auth pages

---
+++ apps/web/app/auth/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex justify-center items-center h-screen w-full flex-col">
+      <main className="max-w-full w-[18rem]">
+        {children}
+      </main>
+    </div>
+  )
+
+}
