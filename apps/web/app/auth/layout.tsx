ec832bc9549d7244a2f4eab946593479c7386670
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-11-01 18:49:36 +0330

    refactor: container max width increase

--- apps/web/app/auth/layout.tsx
+++ apps/web/app/auth/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex justify-center items-center h-screen w-full flex-col">
-      <main className="max-w-full w-[18rem]">
+      <main className="max-w-full w-[20rem]">
         {children}
       </main>
     </div>
