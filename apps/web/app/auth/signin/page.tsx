39bbfedbef4ec356a9a401bbb10ce823ef8079c1
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-11-01 18:50:00 +0330

    refactor: some texts are added to signin page

--- apps/web/app/auth/signin/page.tsx
+++ apps/web/app/auth/signin/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { SigninForm } from "forms/auth";
+import Link from "next/link";
 
 export default function() {
   return (
     <>
-      <h2 className="text-3xl font-bold">Welcome Back!</h2>
-      <p className="text-gray-400 mb-6">To continue please sign in first</p>
+      <h2 className="text-3xl font-bold mb-2">Welcome Back!</h2>
+      <p className="text-gray-400 mb-8">
+        Log in to your account and get back to scheduling meetings with ease.
+      </p>
       <SigninForm />
+      <p className="text-gray-400 text-center text-sm mt-8">Don't have account yet? <Link href="#" className="text-primary-foreground underline">Create now!</Link></p>
     </>
   )
 }
