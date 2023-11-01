9f4caf36ecad4447d907da7d007de2c9ea986b7e
Sepehr Alimohamadi <sepehrinreallife@gmail.com>
2023-11-01 18:26:19 +0330

    chore: export ui components

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,2 +1,7 @@
 import * as React from "react";
+export * from "./components/ui/button";
+export * from "./components/ui/label";
+export * from "./components/ui/form";
+export * from "./components/ui/input";
+export * from "./components/ui/alert";
 
